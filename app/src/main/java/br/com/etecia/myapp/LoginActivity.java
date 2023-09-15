0246236abbd19c5ddbdfa0944b2ae80103d45355
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-09-14 22:04:13 -0300

    Uso das funções java

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,36 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+    //declarar variabel global do tipo do objeto
+    ImageView imgLogo;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        imgLogo = findViewById(R.id.imgLogin);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+
+        imgLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SplashActivity.class));
+            }
+        });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MenuPrincipalActivity.class));
+            }
+        });
     }
 }
